da274e4fcb3908736b4193072c7dd376c2b48129
GeryMolina <g.molina.ojeda@gmail.com>
2019-08-24 23:19:09 -0400

    subi todo menos estilos

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,25 @@
 window.cipher = {
-  encode: () => {
+  encode: (offset, newDream) => {
     /* Acá va tu código */
     
+    let dreamcypher = '';
+    for (let i = 0; i < newDream.length; i++) {
+
+      const dreamcode = newDream.charCodeAt(i);
+      const formula = (dreamcode - 65 + offset) % 26 + 65;
+      dreamcypher += String.fromCharCode(formula);
+    }
+    return dreamcypher;
   },
-  decode: () => {
+  decode: (b, n) => {
     /* Acá va tu código */
+    let decoded = '';
+    for (let i = 0; i < n.length; i++) {
+      const underCode = n.charCodeAt(i);
+      const formula2 = (underCode + 65 - b) % 26 + 65;
+      decoded += String.fromCharCode(formula2);
+    }
+    return decoded;
   }
-};
+
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,69 @@
 /*Acá va tu código*/
+function load() {
+    inicio();
+    choise();
+    addDream();
+    desencripted();
+    final();
+}
+
+
+//pantalla 1
+function inicio() {
+    const startButton = document.getElementById('start');
+    startButton.addEventListener('click', () => {
+        document.getElementById('pantalla2').style.display = 'block';
+        document.getElementById('pantalla1').style.display = 'none';
+
+    })
+}
+
+//pantalla2
+function choise() {
+    const newDreamButton = document.getElementById('newDream');
+    newDreamButton.addEventListener('click', () => {
+        document.getElementById('pantalla3').style.display = 'block';
+
+
+    })
+}
+
+//pantalla 3
+
+function addDream() {
+    const saveDreamButton = document.getElementById('saveDream');
+    saveDreamButton.addEventListener('click', () => {
+        document.getElementById('pantalla4').style.display = 'block';
+        document.getElementById('pantalla2').style.display = 'none';
+        document.getElementById('pantalla3').style.display = 'none';
+        const encriptdream = document.getElementById('dreamcode');
+        const newDream = document.getElementById('addDream').value.toUpperCase();
+        const emotion = document.getElementById('emotionsSelect');
+        const offset = Number(emotion.options[emotion.selectedIndex].value);
+        encriptdream.value = window.cipher.encode(offset, newDream);
+        desencripted(offset, encriptdream.value);
+    })
+}
+
+//pantalla 4
+const decodedDream = document.getElementById('decodedDream');
+function desencripted(b, n) {
+    const backButton = document.getElementById('show');
+    backButton.addEventListener('click', () => {
+        document.getElementById('pantalla5').style.display = 'block';
+        document.getElementById('pantalla4').style.display = 'none';
+        decodedDream.value = window.cipher.decode(b, n);
+    })
+}
+
+function final() {
+    const exitButton = document.getElementById('exit');
+    exitButton.addEventListener('click', () => {
+        document.getElementById('pantalla1').style.display = 'block';
+        document.getElementById('pantalla5').style.display = 'none';
+    })
+}
+
+
+
+document.addEventListener("DOMContentLoaded", load, false)
